d3b05633639ed14e20944546b6f3369ad4c022b2
BalandinaM <marina.balandina.89@mail.ru>
2024-05-24 13:50:19 +0500

    1

--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -46,37 +46,6 @@ const usersReducer = (state = initialState, action) => {
   }
 }
 
-
-// const usersReduser = (state = initialState, action) => {
-//     switch(action.type) {
-//         case FOLLOW:
-//             return {
-//                 ...state,
-//                 usersData: state.usersData.map( u =>  {
-//                     if (u.id === action.userId) {
-//                         return {...u, followed: true}
-//                     }
-//                     return u;
-//                 })
-//             }
-//         case UNFOLLOW:
-//             return {
-//                 ...state,
-//                 usersData: state.usersData.map( u =>  {
-//                     if (u.id === action.userId) {
-//                         return {...u, followed: false}
-//                     }
-//                     return u;
-//                 })
-//             }
-//         case SET_USERS: {
-//             return { ...state, usersData: [ ...state.usersData, ...action.usersData ]}
-//         }
-//         default:
-//             return state;
-//     }
-// }
-
 export const followAC = (userId) => ({ type: FOLLOW, userId });
 export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId });
 export const setUsersAC = (users) => ({ type: SET_USERS, users });

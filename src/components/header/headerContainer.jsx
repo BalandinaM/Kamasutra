6e1a2d67b4f9728ffcf9c7680c70f1027fab5fe5
BalandinaM <marina.balandina.89@mail.ru>
2024-07-05 23:13:52 +0500

    Подгрузка залогиненого профиля

--- src/components/header/headerContainer.jsx
+++ src/components/header/headerContainer.jsx
@@ -3,6 +3,8 @@ import Header from "./header";
 import axios from "axios";
 import { connect } from "react-redux";
 import { setUserData } from '../../redux/authReducer';
+import { setUserProfile } from '../../redux/profileReducer';
+
 
 class HeaderContainer extends React.Component {
   componentDidMount() {
@@ -20,11 +22,30 @@ class HeaderContainer extends React.Component {
         }
       )
       .then((response) => {
+        // debugger;
         if (response.data.resultCode === 0) {
           let { id, login, email } = response.data.data;//днструктуризация чтобы много раз не писать response.data.data.login, response.data.data.email и т.д.
           this.props.setUserData(id, email, login);
+          return id;
         }
-      });
+      })
+      .then((id) => {
+        // debugger;
+        axios
+          .get(
+            `https://social-network.samuraijs.com/api/1.0/profile/`+ id,
+            {
+              headers: {
+                "API-KEY": "8312cd1f-3942-4c8e-84a0-fb6a8c65bd02",
+                Authorization: "Bearer cc8ad68d-99fe-4f27-b6af-cd1b10f297cf",
+              },
+            }
+          )
+          .then((response) => {
+            // debugger;
+            this.props.setUserProfile(response.data);
+          });
+      })
   }
 
   render ()
@@ -38,6 +59,7 @@ class HeaderContainer extends React.Component {
 const mapStateToProps = (state) => ({
       isAuth: state.auth.isAuth,
       login: state.auth.login,
+      profile: state.profilePage.profile,
     })
 
-export default connect(mapStateToProps, {setUserData}) (HeaderContainer);
+export default connect(mapStateToProps, {setUserData, setUserProfile}) (HeaderContainer);
